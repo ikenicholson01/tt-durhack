ee4dc455bf3ca240e5f23897c24a06a93d20982f
ikenicholson01 <ike.nich@icloud.com>
2024-11-02 13:45:10 +0000

    Initial Commit

---
+++ TT-Durhack/TT_DurhackApp.swift
@@ -0,0 +1,17 @@
+//
+//  TT_DurhackApp.swift
+//  TT-Durhack
+//
+//  Created by Ike Nicholson on 02/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct TT_DurhackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
